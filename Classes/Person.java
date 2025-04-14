db7908221253e9134c4ed0adb29d313f0f949141
ammh <67855557+ammrh01@users.noreply.github.com>
2025-04-14 03:59:30 +0000

    Updated Mainclass and Person

--- Classes/Person.java
+++ Classes/Person.java
@@ -1,3 +1,11 @@
 public class Person {
+    private int id;
+    public String name;
+
+    
+    public Person(int ID, String name) {
+        this.id = ID;
+        this.name = name;
+    }
 
 }
--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,7 @@
 public class MainClass {
   public static void main(String[] args) {
-    ddd
+    Person person1 = new Person(23, "Ammar");
+
+    System.out.print(person1.name);
   }
 }
